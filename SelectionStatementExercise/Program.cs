3b4ee2a0020e78bbfddc6a3969a9dc96ef68ea75
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-07-24 16:35:23 -0400

    Created and finished the excersize

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Guess a number: any number...");
+
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
